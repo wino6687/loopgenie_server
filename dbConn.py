9ba64c598caf19ff7cd4401e5836b9639e7325d2
WILLIAM Norris <williamnorris@WILLIAMs-MacBook-Pro-2.local>
2020-05-31 19:04:14 -0600

    updated dbconn to connect to remote psql db

--- dbConn.py
+++ dbConn.py
@@ -1,5 +1,7 @@
-import pymongo
-
+#import pymongo
+import psycopg2
+import config
+'''
 def getTrails(long, lat, rad):
     with pymongo.MongoClient("mongodb://public:public@104.196.106.157:27017/trails?authSource=trails") as client:
         geo = client["trails"]["geo"]
@@ -15,3 +17,21 @@ def getTrails(long, lat, rad):
             except IndexError as e:
                 continue
         return toReturn
+'''
+
+def getTrails_sql(long, lat, rad):
+    """
+    query trails from new postGIS db
+    """
+    conn = psycopg2.connect(user = config.post_user,
+                                    password = config.post_password,
+                                    port = '5432',
+                                    database = 'trails')
+    cursor = conn.cursor()
+    select_query = "SELECT name, wkb_geometry FROM geom WHERE ST_DWithin(wkb_geometry, ST_MakePoint({}, {})::geography, {});".format(long, lat, rad)
+    cursor.execute(select_query)
+    trails = cursor.fetchall()
+    return trails
+    
+
+
