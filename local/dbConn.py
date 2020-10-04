98e3cb2146094a3aa0bc59124c9cc9d06276c9ba
wino6687 <wino6687@colorado.edu>
2020-10-04 09:22:09 -0600

    adding local folder it reflects what we are using in lambda

---
+++ local/dbConn.py
@@ -0,0 +1,21 @@
+import psycopg2
+import config
+
+
+def getTrails_sql(long, lat, rad):
+    """
+    query trails from new postGIS db
+    """
+    conn = psycopg2.connect(user = config.post_user,
+                                    password = config.post_password,
+                                    port = '5432',
+                                    database = 'trails',
+                                    host= 'trails.ctluwc1bi2yb.us-east-2.rds.amazonaws.com')
+    cursor = conn.cursor()
+    select_query = "SELECT name, wkb_geometry FROM geom WHERE ST_DWithin(wkb_geometry, ST_MakePoint({}, {})::geography, {});".format(long, lat, rad)
+    cursor.execute(select_query)
+    trails = cursor.fetchall()
+    return trails
+    
+
+
